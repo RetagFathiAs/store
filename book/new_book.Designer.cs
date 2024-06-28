b419f3f0aae4e929d908c1d882347bfc6bcad7b6
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-06-28 19:03:12 +0200

    Add files via upload

---
+++ book/new_book.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace book
+{
+  partial class new_book
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(new_book));
+      this.comboBox1 = new System.Windows.Forms.ComboBox();
+      this.classnumtex = new System.Windows.Forms.TextBox();
+      this.label4 = new System.Windows.Forms.Label();
+      this.booknametex = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.booknumtex = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.panel1 = new System.Windows.Forms.Panel();
+      this.button3 = new System.Windows.Forms.Button();
+      this.button1 = new System.Windows.Forms.Button();
+      this.textBox_image_path = new System.Windows.Forms.TextBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.button2 = new System.Windows.Forms.Button();
+      this.panel2 = new System.Windows.Forms.Panel();
+      this.panel1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.panel2.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // comboBox1
+      // 
+      this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+      this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.comboBox1.FormattingEnabled = true;
+      this.comboBox1.Location = new System.Drawing.Point(112, 544);
+      this.comboBox1.Name = "comboBox1";
+      this.comboBox1.Size = new System.Drawing.Size(265, 44);
+      this.comboBox1.TabIndex = 16;
+      // 
+      // classnumtex
+      // 
+      this.classnumtex.BackColor = System.Drawing.Color.White;
+      this.classnumtex.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.classnumtex.Location = new System.Drawing.Point(112, 609);
+      this.classnumtex.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+      this.classnumtex.Name = "classnumtex";
+      this.classnumtex.Size = new System.Drawing.Size(263, 41);
+      this.classnumtex.TabIndex = 14;
+      // 
+      // label4
+      // 
+      this.label4.AutoSize = true;
+      this.label4.BackColor = System.Drawing.Color.Thistle;
+      this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.label4.Location = new System.Drawing.Point(408, 609);
+      this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(264, 44);
+      this.label4.TabIndex = 13;
+      this.label4.Text = "رقم السنة الدراسية";
+      // 
+      // booknametex
+      // 
+      this.booknametex.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.booknametex.Location = new System.Drawing.Point(112, 474);
+      this.booknametex.Name = "booknametex";
+      this.booknametex.Size = new System.Drawing.Size(265, 41);
+      this.booknametex.TabIndex = 15;
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.BackColor = System.Drawing.Color.Thistle;
+      this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.label3.Location = new System.Drawing.Point(436, 544);
+      this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(208, 44);
+      this.label3.TabIndex = 12;
+      this.label3.Text = "السنة الدراسية";
+      // 
+      // booknumtex
+      // 
+      this.booknumtex.BackColor = System.Drawing.Color.White;
+      this.booknumtex.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.booknumtex.Location = new System.Drawing.Point(112, 404);
+      this.booknumtex.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+      this.booknumtex.Name = "booknumtex";
+      this.booknumtex.Size = new System.Drawing.Size(263, 41);
+      this.booknumtex.TabIndex = 10;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.BackColor = System.Drawing.Color.Thistle;
+      this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.label1.Location = new System.Drawing.Point(455, 404);
+      this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(158, 44);
+      this.label1.TabIndex = 9;
+      this.label1.Text = "رقم الكتاب";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.BackColor = System.Drawing.Color.Thistle;
+      this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+      this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.label2.Location = new System.Drawing.Point(453, 474);
+      this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(160, 44);
+      this.label2.TabIndex = 11;
+      this.label2.Text = "اسم الكتاب";
+      // 
+      // panel1
+      // 
+      this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+      this.panel1.Controls.Add(this.button3);
+      this.panel1.Controls.Add(this.comboBox1);
+      this.panel1.Controls.Add(this.classnumtex);
+      this.panel1.Controls.Add(this.label4);
+      this.panel1.Controls.Add(this.button1);
+      this.panel1.Controls.Add(this.booknametex);
+      this.panel1.Controls.Add(this.label3);
+      this.panel1.Controls.Add(this.booknumtex);
+      this.panel1.Controls.Add(this.label1);
+      this.panel1.Controls.Add(this.label2);
+      this.panel1.Location = new System.Drawing.Point(231, 0);
+      this.panel1.Name = "panel1";
+      this.panel1.Size = new System.Drawing.Size(725, 785);
+      this.panel1.TabIndex = 17;
+      // 
+      // button3
+      // 
+      this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+      this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.button3.Location = new System.Drawing.Point(24, 712);
+      this.button3.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+      this.button3.Name = "button3";
+      this.button3.Size = new System.Drawing.Size(144, 50);
+      this.button3.TabIndex = 37;
+      this.button3.Text = "خروج";
+      this.button3.UseVisualStyleBackColor = true;
+      this.button3.Click += new System.EventHandler(this.button3_Click);
+      // 
+      // button1
+      // 
+      this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+      this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+      this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button1.Location = new System.Drawing.Point(309, 677);
+      this.button1.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(160, 72);
+      this.button1.TabIndex = 36;
+      this.button1.Text = "حفظ";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // textBox_image_path
+      // 
+      this.textBox_image_path.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.textBox_image_path.Location = new System.Drawing.Point(76, 14);
+      this.textBox_image_path.Name = "textBox_image_path";
+      this.textBox_image_path.Size = new System.Drawing.Size(207, 41);
+      this.textBox_image_path.TabIndex = 33;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Location = new System.Drawing.Point(892, 23);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(435, 409);
+      this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+      this.pictureBox1.TabIndex = 34;
+      this.pictureBox1.TabStop = false;
+      // 
+      // button2
+      // 
+      this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2.Location = new System.Drawing.Point(106, 78);
+      this.button2.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(149, 63);
+      this.button2.TabIndex = 35;
+      this.button2.Text = "غلاف الكتاب";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // panel2
+      // 
+      this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+      this.panel2.Controls.Add(this.textBox_image_path);
+      this.panel2.Controls.Add(this.button2);
+      this.panel2.Location = new System.Drawing.Point(941, 460);
+      this.panel2.Name = "panel2";
+      this.panel2.Size = new System.Drawing.Size(479, 217);
+      this.panel2.TabIndex = 36;
+      // 
+      // new_book
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(1362, 777);
+      this.Controls.Add(this.panel2);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.panel1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "new_book";
+      this.Text = "new_book";
+      this.Load += new System.EventHandler(this.new_book_Load);
+      this.panel1.ResumeLayout(false);
+      this.panel1.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.panel2.ResumeLayout(false);
+      this.panel2.PerformLayout();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ComboBox comboBox1;
+    private System.Windows.Forms.TextBox classnumtex;
+    private System.Windows.Forms.Label label4;
+    private System.Windows.Forms.TextBox booknametex;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.TextBox booknumtex;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.TextBox textBox_image_path;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Panel panel2;
+  }
+}
